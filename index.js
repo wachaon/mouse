32166bdcfb89a23cd9a68beba1aa5b71712dcdcd
wachaon@gmail.com <wachaon@gmail.com>
2023-02-26 15:53:42 +0900

    Change the content to match the csharpscript module

--- index.js
+++ index.js
@@ -1,68 +1,62 @@
-const { readFileSync, existsFileSync } = require('filesystem')
-const { resolve, basename, extname } = require('pathname')
+const { existsFileSync } = require('filesystem')
+const { resolve } = require('pathname')
 const { execCommand } = require('utility')
-const ps = require('ps')
+const argv = require('argv')
+const isCLI = require('isCLI')
+const { execScript, compile } = require('csharpscript')
 
 // mouse
-const mouse_cs = generate('src/mouse.cs', 3)
+const mouse_cs = resolve(__dirname, 'src/mouse.cs')
 const mouse_exe = resolve(__dirname, 'mouse.exe')
 const exists_mouse_exe = existsFileSync(mouse_exe)
+const mouse = 'mouse'
+const Main = 'Main'
 
 // mouse method
 function pos(x = 0, y = 0) {
     if (exists_mouse_exe) execCommand(`${mouse_exe} pos ${x} ${y}`)
-    else ps(mouse_cs, ['pos', x, y])
+    else execScript(mouse_cs, mouse, Main, 'pos', x, y)
 }
 
 function click() {
     if (exists_mouse_exe) execCommand(`${mouse_exe} click`)
-    else ps(mouse_cs, ['click', 0, 0])
+    else execScript(mouse_cs, mouse, Main, 'click', 0, 0)
 }
 
 function leftDown() {
     if (exists_mouse_exe) execCommand(`${mouse_exe} leftDown`)
-    else ps(mouse_cs, ['leftDown', 0, 0])
+    else execScript(mouse_cs, mouse, Main, 'leftDown', 0, 0)
 }
 
 function leftUp() {
     if (exists_mouse_exe) execCommand(`${mouse_exe} leftUp`)
-    else ps(mouse_cs, ['leftUp', 0, 0])
+    else execScript(mouse_cs, mouse, Main, 'leftUp', 0, 0)
 }
 
 function rightClick() {
     if (exists_mouse_exe) execCommand(`${mouse_exe} rightClick`)
-    else ps(mouse_cs, ['rightClick', 0, 0])
+    else execScript(mouse_cs, mouse, Main, 'rightClick', 0, 0)
 }
 
 function rightDown() {
     if (exists_mouse_exe) execCommand(`${mouse_exe} rightDown`)
-    else ps(mouse_cs, ['rightDown', 0, 0])
+    else execScript(mouse_cs, mouse, Main, 'rightDown', 0, 0)
 }
 
 function rightUp() {
     if (exists_mouse_exe) execCommand(`${mouse_exe} rightUp`)
-    else ps(mouse_cs, ['rightUp', 0, 0])
+    elseexecScript(mouse_cs, mouse, Main, 'rightUp', 0, 0)
 }
 
 function scroll(movement = 0) {
     if (exists_mouse_exe) execCommand(`${mouse_exe} scroll ${movement}`)
-    else ps(mouse_cs, ['scroll', movement, 0])
+    else execScript(mouse_cs, mouse, Main, 'scroll', movement, 0)
 }
 
-function generate(spec, len = 0) {
-    const file = resolve(__dirname, spec)
-    const program = basename(file, extname(file))
-    const args = len ? (new Array(len)).fill(0).map((arg, i) => `$args[${i}]`).join(', ') : ''
-    const source = readFileSync(file, 'auto')
-    const code = `$Source = @"
-${source}"@
 
-Add-Type -Language CSharp -TypeDefinition $Source
-[${program}]::Main(${args})`
-    return code
-}
-
-module.exports = {
+if (isCLI(__filename)) {
+    if (argv.get('c') || argv.get('compile') || argv.unnamed[1] === 'compile') compile(mouse_cs, { out: mouse_exe })
+} else module.exports = {
     pos,
     click,
     leftDown,
